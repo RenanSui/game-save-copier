846aa424ca6b1092487b94d20b8cdc8882843664
renansui <renanddtao@gmail.com>
2022-10-22 04:10:40 -0300

    changed documents directory to desktop on destination

--- saveCopier.js
+++ saveCopier.js
@@ -7,11 +7,11 @@ const computerName = os.homedir();
 
 // PERSONA 5 PATHS
 const srcDir = `${computerName}/AppData/Local/Packages/SEGAofAmericaInc.F0cb6b3aer_s751p9cej88mt/SystemAppData`;
-const destDir = `${computerName}/documents/Pasta com saves de jogos/Persona 5 Royal/SystemAppData`;
+const destDir = `${computerName}/desktop/Pasta com saves de jogos/Persona 5 Royal/SystemAppData`;
 
 // DARK SOULS III PATHS
 // const srcDir = `${computerName}/AppData/Roaming/DarkSoulsIII`;
-// const destDir = `${computerName}/documents/Pasta com saves de jogos/DarkSoulsIII`;
+// const destDir = `${computerName}/desktop/Pasta com saves de jogos/DarkSoulsIII`;
 
 // To copy a folder or file, select overwrite accordingly
 const copyFolders = () => {
